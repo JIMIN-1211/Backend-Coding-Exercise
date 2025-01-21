933b4858653e84bd1e707d1bf6015f5784f215df
Jimin Pyo <dkwlrhdwn20@gmail.com>
2025-01-21 20:05:54 +0900

    import-export-practice

--- React/289-es6-import-export-practice-start/289-es6-import-export-practice/src/index.jsx
+++ React/289-es6-import-export-practice-start/289-es6-import-export-practice/src/index.jsx
@@ -1,18 +1,20 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import {createRoot} from "react-dom/client";
+import * as cal from "./calculator.js";
 
 //Import the add, multiply, subtract and divide functions
 //from the calculator.js file.
 //If successful, your website should look the same as the Final.png
 
-ReactDOM.render(
+const root = createRoot(document.getElementById("root"));
+
+root.render(
   <ul>
-    <li>{add(1, 2)}</li>
-    <li>{multiply(2, 3)}</li>
-    <li>{subtract(7, 2)}</li>
-    <li>{divide(5, 2)}</li>
-  </ul>,
-  document.getElementById("root")
+    <li>{cal.add(1, 2)}</li>
+    <li>{cal.multiply(2, 3)}</li>
+    <li>{cal.subtract(7, 2)}</li>
+    <li>{cal.divide(5, 2)}</li>
+  </ul>
 );
 
 // If you're running this locally in VS Code use the commands:
